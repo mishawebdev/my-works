462310772d5d820052238afb4bd120dca11cf7ea
Misha Serdyuk <50199653+mishawebdev@users.noreply.github.com>
2024-08-09 17:29:47 +0300

    Add files via upload

---
+++ shelter/js/script.js
@@ -0,0 +1,3 @@
+window.addEventListener('scroll', function () {
+	scrollY > 0 ? document.querySelector('.header').classList.add('scroll') : document.querySelector('.header').classList.remove('scroll');
+});
